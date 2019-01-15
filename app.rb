62ce0ac332e0e6a53dab2e8cbd0f7f91b3b086f3
clifford <clifford@noukod.com>
2019-01-15 14:29:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get '/reversename/:name' do{
+  get '/reversename/:name' do
     @name= params[:name]
     erb :reversename
-    
-  }
+  
 end
