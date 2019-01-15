9bfd02dd34dd0d3bd8b1be669d292b6854d149e1
clifford <clifford@noukod.com>
2019-01-15 15:34:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -42,12 +42,16 @@ class App < Sinatra::Base
        @w5 = params[:word5]
        "#{@w1} #{@w2} #{@w3} #{@w4} #{@w5}"
      end
-  
+
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
-    
+
+    case @operation
+    when "subtract"
+      (@num1-@num2).to_s
+
   end
 
 end
