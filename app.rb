a65cdba9912bcf203b755d2e594705b6b0e9dc5e
clifford <clifford@noukod.com>
2019-01-15 15:42:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -43,20 +43,19 @@ class App < Sinatra::Base
        "#{@w1} #{@w2} #{@w3} #{@w4} #{@w5}"
      end
 
-  get '/:operation/:number1/:number2' do
-    @operat = params[:operation]
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
-
-    case @operat
-      when "subtract"
-        (@num1 - @num2).to_s
-      when "add"
-        (@num1 + @num2).to_s
-      when "multiply"
-        (@num1 * @num2).to_s
-      when "divide"
-        (@num1 / @num2).to_s
-      end
+     get '/:operation/:number1/:number2' do
+       @oper = params[:operation]
+       @num1 = params[:number1].to_i
+       @num2 = params[:number2].to_i
+       case @oper
+         when "subtract"
+           (@num1 - @num2).to_s
+         when "add"
+           (@num1 + @num2).to_s
+         when "multiply"
+           (@num1 * @num2).to_s
+         when "divide"
+           (@num1 / @num2).to_s
+       end
   end
 end
