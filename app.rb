63766c6d2111145b4e6f429ddcb33bcba73f6cd7
clifford <clifford@noukod.com>
2019-01-15 15:07:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,6 +18,7 @@ class App < Sinatra::Base
     get '/say/:number/:phrase' do
       @number = params[:number].to_i
       @phrase = params[:phrase]
+      "#{@phrase.reverse}"
   #     erb :say
   #   end
   #
