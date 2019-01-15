71697fe53ee531b382ac0e5fb5647ef055dee231
clifford <clifford@noukod.com>
2019-01-15 15:12:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,12 +22,8 @@ class App < Sinatra::Base
     # end
 
     get "/say/:number/:phrase" do 
-      result = ""
-      number.to_i.times do |i|
-      result += phrase.upcase
-      end
-      result
-    end 
+    phrase * number.to_i
+  end
   #
   #   get '/say/:word1/:word2/:word3/:word4/:word5' do
   #     @w1 = params[:word1]
